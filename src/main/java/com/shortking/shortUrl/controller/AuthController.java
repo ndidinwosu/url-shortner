8b7fbc5b1c98f093823f9ce3b02ec9c3609c4892
Ndidi Nwosu <ndidinwosu@gmail.com>
2025-04-07 15:11:06 -0500

    user not users

--- src/main/java/com/shortking/shortUrl/controller/AuthController.java
+++ src/main/java/com/shortking/shortUrl/controller/AuthController.java
@@ -22,7 +22,7 @@ import io.swagger.annotations.Example;
 import io.swagger.annotations.ExampleProperty;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class AuthController {
 
     @Autowired

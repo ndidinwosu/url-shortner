5209a2e46de46c4b4f34de819ffac363b457f1dd
Yanyucheng <673816542@qq.com>
2025-04-07 14:32:22 -0500

    Update AuthController.java

--- src/main/java/com/shortking/shortUrl/controller/AuthController.java
+++ src/main/java/com/shortking/shortUrl/controller/AuthController.java
@@ -17,8 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+
 import io.swagger.annotations.Example;
 import io.swagger.annotations.ExampleProperty;
 

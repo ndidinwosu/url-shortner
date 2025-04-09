4f1c495498c4778f0c44c24f9fb79aa7fcf5e32b
kokomi <994304474@qq.com>
2025-04-09 16:52:58 -0500

    feat:sending reset password email

--- src/main/java/com/shortking/shortUrl/service/EmailService.java
+++ src/main/java/com/shortking/shortUrl/service/EmailService.java
@@ -1,12 +1,23 @@
 package com.shortking.shortUrl.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
     public void send(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("midnightneon2001@gmail.com"); // 发件人必须与配置一致
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
 
+        mailSender.send(message);
         System.out.println("=== Sending Email ===");
         System.out.println("To: " + to);
         System.out.println("Subject: " + subject);
